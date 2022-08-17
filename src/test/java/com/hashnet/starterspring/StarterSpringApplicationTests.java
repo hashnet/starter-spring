70e5338a5c6104e801241e4de2350a1587ea63e8
Maidul Hasan <maidul.hasan@gmail.com>
2022-08-18 02:54:07 +1000

    Initial Commit

---
+++ src/test/java/com/hashnet/starterspring/StarterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashnet.starterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
